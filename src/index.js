7c02ac3f62a26d47dc53822b5d5725577f421005
esaharov <esaharov@osslabs.ru>
2018-09-18 11:00:29 +0400

    SLAVA VILLAGE MAN

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+	return (
+		<div>Hello Oleg! How are you?</div>
+	);
+}
+
+ReactDOM.render(<Index />, document.getElementById("metka"));
